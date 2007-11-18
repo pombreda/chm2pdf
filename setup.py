e519a9e8a448e74ac4363c523a0c5343fc9cd7b6
devicerandom <devicerandom@2dbe0753-dd35-0410-9897-cd82072108a1>
2007-11-18 14:02:08 +0000

    Updated to version 0.9 by Chris Karakas

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(name = "chm2pdf",
-    version = "0.0.3",
+    version = "0.9",
     description = "A script to convert CHM files into PDF",
-    author = "Massimo Sandal",
-    author_email = "devicerandom@gmail.com",
+    author = "Massimo Sandal, Chris Karakas",
+    author_email = "devicerandom@gmail.com, chris@karakas-online.de",
     url = "http://code.google.com/p/chm2pdf/",
     scripts = ["chm2pdf"],
-    long_description = "A script to convert CHM files into PDF. Requires chmlib, pychm, htmldoc, pdftk."
+    long_description = "A script to convert CHM files into PDF. Requires chmlib, pychm, htmldoc."
     )
     
 
